ff0024f4b123cff8ca58e53d22b3b7d2d95d2c81
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-06 22:01:41 +0100

    rearrange numpad console log the operators

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('hello world')
+function workingButton() {
+    alert("This button is working")
+}
+
+function add(a, b) {
+    return a + b; 
+}
+
+function substract(a, b) {
+    return a - b; 
+}
+
+function multiply(a, b) {
+    return a * b; 
+}
+
+function divide(a, b) {
+    return a / b; 
+}
+
+let firstNumber = 0; 
+let operator = '';
+let secondNumber = 0; 
+
+function operate(o, a, b) {
+    if (o == 'add') {
+        console.log('add')
+    } else if (o == 'substract') {
+        console.log('substract')
+    } else if (o == 'divide') {
+        console.log('divide')
+    } else if (o == 'multiply') {
+        console.log('multiply')
+    }
+}
+
+
+
